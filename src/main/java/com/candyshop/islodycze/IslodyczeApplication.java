279b17a82b2e5ed5786151566df3849248e43d6d
Mateusz Wołek <sekwoja5@gmail.com>
2022-10-13 19:32:20 +0200

    Spring boot project initialization

---
+++ src/main/java/com/candyshop/islodycze/IslodyczeApplication.java
@@ -0,0 +1,13 @@
+package com.candyshop.islodycze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslodyczeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslodyczeApplication.class, args);
+	}
+
+}
